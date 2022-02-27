dd7b54b5fdb6d62a1984bdbce633b8bf3e94af90
Amrillo <amrishka88@gmail.com>
2022-02-27 19:14:13 +0500

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    let res = n.toString().split("").reverse().map(Number);
+    return (res[0]== 0)? res.slice(1).filter(elem=> elem >=0).join("") : res.filter(elem=> elem >=0).join("");
 }
+
